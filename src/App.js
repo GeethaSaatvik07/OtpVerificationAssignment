865d01e6431f2c020bb0326c5508d150381f1d4b
GeethaSaatvik07 <adepusaatvik@protonmail.com>
2023-09-26 12:59:39 +0000

    fully working verification otp

--- src/App.js
+++ src/App.js
@@ -1,19 +1,17 @@
-import { BsFillShieldLockFill, BsTelephoneFill } from "react-icons/bs";
-import { CgSpinner } from "react-icons/cg";
-
 import OtpInput from "otp-input-react";
 import { useState } from "react";
 import PhoneInput from "react-phone-input-2";
-import "react-phone-input-2/lib/style.css";
+import "react-phone-input-2/lib/material.css";
 import { auth } from "./firebase.config";
 import { RecaptchaVerifier, signInWithPhoneNumber } from "firebase/auth";
 import { toast, Toaster } from "react-hot-toast";
 
+import WelcomePage from "./components/WelcomePage";
+
 const App = () => {
   const [otp, setOtp] = useState("");
   const [ph, setPh] = useState("");
-  const [loading, setLoading] = useState(false);
-  const [showOTP, setShowOTP] = useState(false);
+  const [showOTP, setShowOTP] = useState(true);
   const [user, setUser] = useState(null);
 
   function onCaptchVerify() {
@@ -33,7 +31,6 @@ const App = () => {
   }
 
   function onSignup() {
-    setLoading(true);
     onCaptchVerify();
 
     const appVerifier = window.recaptchaVerifier;
@@ -43,56 +40,60 @@ const App = () => {
     signInWithPhoneNumber(auth, formatPh, appVerifier)
       .then((confirmationResult) => {
         window.confirmationResult = confirmationResult;
-        setLoading(false);
         setShowOTP(true);
         toast.success("OTP sended successfully!");
       })
       .catch((error) => {
         console.log(error);
-        setLoading(false);
       });
   }
 
   function onOTPVerify() {
-    setLoading(true);
     window.confirmationResult
       .confirm(otp)
       .then(async (res) => {
         console.log(res);
         setUser(res.user);
-        setLoading(false);
       })
       .catch((err) => {
         console.log(err);
-        setLoading(false);
       });
   }
 
+  function changePhoneNumber() {
+    setShowOTP(false);
+  }
+
   return (
-    <section className="bg-emerald-500 flex items-center justify-center h-screen">
+    <section className="bg-white flex items-center justify-center h-screen">
       <div>
         <Toaster toastOptions={{ duration: 4000 }} />
         <div id="recaptcha-container"></div>
         {user ? (
-          <h2 className="text-center text-white font-medium text-2xl">
-            👍Login Success
-          </h2>
+          <WelcomePage />
         ) : (
-          <div className="w-80 flex flex-col gap-4 rounded-lg p-4">
-            <h1 className="text-center leading-normal text-white font-medium text-3xl mb-6">
-              Welcome to <br /> CODE A PROGRAM
-            </h1>
+          <div className="w-80 flex flex-col gap-4 rounded-lg">
             {showOTP ? (
-              <>
-                <div className="bg-white text-emerald-500 w-fit mx-auto p-4 rounded-full">
-                  <BsFillShieldLockFill size={30} />
-                </div>
-                <label
-                  htmlFor="otp"
-                  className="font-bold text-xl text-white text-center"
+              <div className="verify-page">
+                <img
+                  src="https://res.cloudinary.com/dbj5bk2gm/image/upload/v1695661317/undraw_confirmed_81ex_zppqt3.svg"
+                  alt="otp check"
+                  className="verify-otp-check-image"
+                />
+                <h1 className="verify-mobile-line">
+                  Please verify Mobile number
+                </h1>
+                <p className="verify-mobile-otp-sent-line">
+                  An OTP is sent to{" "}
+                  <span className="mobile-number-verify">+917896781234</span>
+                </p>
+                <button
+                  type="button"
+                  onClick={changePhoneNumber}
+                  className="verify-change-phone-number"
                 >
-                  Enter your OTP
-                </label>
+                  Change Phone Number
+                </button>
                 <OtpInput
                   value={otp}
                   onChange={setOtp}
@@ -102,38 +103,70 @@ const App = () => {
                   autoFocus
                   className="opt-container "
                 ></OtpInput>
+                <div className="verify-otp-not-received">
+                  <p className="verify-not-received">
+                    Didn’t receive the code?
+                  </p>
+                  <button
+                    type="button"
+                    className="verify-otp-resend"
+                    onClick={onSignup}
+                  >
+                    Resend
+                  </button>
+                </div>
                 <button
                   onClick={onOTPVerify}
-                  className="bg-emerald-600 w-full flex gap-1 items-center justify-center py-2.5 text-white rounded"
+                  type="button"
+                  className="verify-button"
                 >
-                  {loading && (
-                    <CgSpinner size={20} className="mt-1 animate-spin" />
-                  )}
-                  <span>Verify OTP</span>
+                  Verify
                 </button>
-              </>
+              </div>
             ) : (
-              <>
-                <div className="bg-white text-emerald-500 w-fit mx-auto p-4 rounded-full">
-                  <BsTelephoneFill size={30} />
+              <div className="number-page">
+                <img
+                  src="https://res.cloudinary.com/dbj5bk2gm/image/upload/v1695661317/AK_logo_beduvs.jpg"
+                  alt="admitkard logo"
+                  className="number-page-logo"
+                />
+                <h1 className="number-page-heading">Welcome Back</h1>
+                <p className="number-back-sign-in-line">
+                  Please sign in to your account
+                </p>
+                <div>
+                  <PhoneInput
+                    specialLabel={"Enter Contact Number"}
+                    country={"in"}
+                    enableSearch={true}
+                    countryCodeEditable={false}
+                    value={ph}
+                    onChange={(ph) => setPh(ph)}
+                    className="box"
+                  />
                 </div>
-                <label
-                  htmlFor=""
-                  className="font-bold text-xl text-white text-center"
-                >
-                  Verify your phone number
-                </label>
-                <PhoneInput country={"in"} value={ph} onChange={setPh} />
-                <button
-                  onClick={onSignup}
-                  className="bg-emerald-600 w-full flex gap-1 items-center justify-center py-2.5 text-white rounded"
-                >
-                  {loading && (
-                    <CgSpinner size={20} className="mt-1 animate-spin" />
-                  )}
-                  <span>Send code via SMS</span>
+                <p className="number-page-otp-note">
+                  We will send you a one time SMS message. Charges may apply.
+                </p>
+                {/* {phone.length === 12 ? (
+                  <Link to="/verify">
+                    <button
+                      className="number-page-sign-in-button"
+                      type="button"
+                      onClick={onSignUp}
+                    >
+                      Sign In with OTP
+                    </button>
+                  </Link>
+                ) : (
+                  <button className="number-page-sign-in-button" type="button">
+                    Sign In with OTP
+                  </button>
+                )} */}
+                <button className="number-page-sign-in-button" type="button">
+                  Sign In with OTP
                 </button>
-              </>
+              </div>
             )}
           </div>
         )}
--- src/firebase.config.js
+++ src/firebase.config.js
@@ -7,13 +7,13 @@ import { getAuth } from "firebase/auth";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "",
-  authDomain: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: "",
-  appId: "",
-  measurementId: "",
+  apiKey: "AIzaSyDX17r3kkfpSvUNxJby7hrqGFjAEab034o",
+  authDomain: "otpcheck-d7eb4.firebaseapp.com",
+  projectId: "otpcheck-d7eb4",
+  storageBucket: "otpcheck-d7eb4.appspot.com",
+  messagingSenderId: "446137818045",
+  appId: "1:446137818045:web:2e939b8f6a7d06f654d0d4",
+  measurementId: "G-PMGZZZ83ZJ",
 };
 
 // Initialize Firebase
